ae1aea7650860f752c692bd76537edddcebff05f
RS Business Tech <rsbusinesstech@gmail.com>
2025-11-05 22:16:43 +0530

    Fixing build failure issue

--- src/app/features/mm2h/mm2h.module.ts
+++ src/app/features/mm2h/mm2h.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { Mm2hComponent } from './components/mm2h/mm2h.component';
-import { Mm2hRoutingModule } from './Mm2hRoutingModule';
+import { Mm2hRoutingModule } from './mm2hRoutingModule';
 
 @NgModule({
   declarations: [Mm2hComponent],
