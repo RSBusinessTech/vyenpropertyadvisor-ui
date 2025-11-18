acd65704d0e7c4aa9622d0621abe846558bd54a7
RS Business Tech <rsbusinesstech@gmail.com>
2025-11-18 22:24:28 +0530

    Adding Download button

--- src/app/features/mm2h/components/mm2h/mm2h.component.ts
+++ src/app/features/mm2h/components/mm2h/mm2h.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Property } from 'src/app/model/property';
 import { PropertyService } from 'src/app/services/property.service';
+import html2canvas from 'html2canvas';
+import jsPDF from 'jspdf';
 
 @Component({
   selector: 'app-mm2h',
@@ -44,4 +46,30 @@ export class Mm2hComponent implements OnInit {
     alert('Sharing not supported on this browser. You can copy the URL manually.');
   }
  }
+
+ //Download PDF/
+  downloadPDF() {
+  const DATA: any = document.getElementById('mm2h-table-section');
+  if (!DATA) {
+    console.error('MM2H table section not found!');
+    return;
+  }
+
+  html2canvas(DATA).then((canvas) => {
+    const imgData = canvas.toDataURL('image/png');
+    const pdf = new jsPDF('p', 'mm', 'a4');
+
+    const imgWidth = 210; // full width A4
+    const pageHeight = 297;
+    const imgHeight = canvas.height * imgWidth / canvas.width;
+
+    let position = 0;
+    pdf.addImage(imgData, 'PNG', 0, position, imgWidth, imgHeight);
+    pdf.save('MM2H_Table.pdf');
+  }).catch((err) => {
+    console.error('Error generating PDF:', err);
+  });
+}
+
+
 }
