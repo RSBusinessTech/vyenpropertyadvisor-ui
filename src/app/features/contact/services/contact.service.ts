7c0f35f15cf7c54642fe17d407da108d7b808cb9
RS Business Tech <rsbusinesstech@gmail.com>
2026-04-05 20:53:30 +0530

    fixing contact section

--- src/app/features/contact/services/contact.service.ts
+++ src/app/features/contact/services/contact.service.ts
@@ -9,8 +9,8 @@ import { Observable } from 'rxjs';
 export class ContactService {
 
   //URL of Rest-API to be called to send email.
-  private url = "https://rsbusinesstech-backend.onrender.com/contact/sendEmail";
-  //private url = "http://127.0.0.1:8080/contact/sendEmail";
+  private url = "https://rsbusinesstech-backend.onrender.com/contact/sendEmailVyenPropertyAdvisor";
+  //private url = "http://127.0.0.1:8080/contact/sendEmailVyenPropertyAdvisor";
 
   //Dependency Injection of HttpClient(Constrcutor DI).
   constructor(private httpClient: HttpClient) { }
