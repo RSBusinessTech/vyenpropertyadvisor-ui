5f41e12fbc5c38186e4c64975f397ce110e91313
RS BusinessTech <rsbusinesstech@gmail.com>
2026-04-26 15:36:14 +0530

    enabled commerical property

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -25,7 +25,6 @@ import { FormsModule } from '@angular/forms';
     AppRoutingModule,
     FormsModule,
     HttpClientModule,  //HttpClient module.
-
   ],
   providers: [],
   bootstrap: [AppComponent]
